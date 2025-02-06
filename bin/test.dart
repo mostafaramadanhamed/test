a37077551f3a48fb49ee2ee35703f277762f6121
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-06 09:47:46 +0200

    rtgsa

--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,12 @@
 void main(){
   print('play');
+  // non nullable
+  int i;
+  // nullable variable
+  int ? t;
+  // variable with initial value
+  int e=0;
+  int lineCount;
+  //var _ = 1;
+  int _ = 2;
 }
