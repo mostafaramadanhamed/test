c52e13cff4fb41e36d9d90cad2a6df272dd8e3d2
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-07 10:21:30 +0200

    operators

--- bin/test.dart
+++ bin/test.dart
@@ -10,3 +10,23 @@ void main(){
   //var _ = 1;
   int _ = 2;
 }
+
+/*
+unary postfix	expr++    expr--    ()    []    ?[]    .    ?.    !	None
+unary prefix	-expr    !expr    ~expr    ++expr    --expr      await expr	None
+multiplicative	*    /    %  ~/	Left
+additive	+    -	Left
+shift	<<    >>    >>>	Left
+bitwise AND	&	Left
+bitwise XOR	^	Left
+bitwise OR	|	Left
+relational and type test	>=    >    <=    <    as    is    is!	None
+equality	==    !=	None
+logical AND	&&	Left
+logical OR	||	Left
+if-null	??	Left
+conditional	expr1    ?    expr2    :    expr3	Right
+cascade	..    ?..	Left
+assignment	=    *=    /=   +=   -=   &=   ^=   etc.	Right
+spread (See note)	...    ...?	None
+ */
