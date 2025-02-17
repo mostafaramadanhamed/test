b7c3a329de1f3bf836633c63949bd94b1bae9984
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-17 10:45:18 +0200

    - branches

---
+++ bin/control_flow.dart
@@ -0,0 +1,31 @@
+// branches
+// if
+// if (isRaining()) {
+//   you.bringRainCoat();
+// } else if (isSnowing()) {
+//   you.wearJacket();
+// } else {
+//   car.putTopDown();
+// }
+// if case
+// if (pair case [int x, int y]) {
+//   print('Was coordinate array $x,$y');
+// } else {
+//   throw FormatException('Invalid coordinates.');
+// }
+// switch
+// Empty cases fall through to the next case, allowing cases to share a body. For an empty case that does not fall through, use break for its body. For non-sequential fall-through, you can use a continue statement and a label:
+//
+// switch (command) {
+//   case 'OPEN':
+//     executeOpen();
+//     continue newCase; // Continues executing at the newCase label.
+//
+//   case 'DENIED': // Empty case falls through.
+//   case 'CLOSED':
+//     executeClosed(); // Runs for both DENIED and CLOSED,
+//
+//   newCase:
+//   case 'PENDING':
+//     executeNowClosed(); // Runs for both OPEN and PENDING.
+// }
