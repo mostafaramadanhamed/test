4e1d2441f8ee87db0824019b526b0bc2cef89285
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-20 10:08:07 +0200

    enum

---
+++ bin/enum.dart
@@ -0,0 +1,27 @@
+// enum
+// declare
+enum Color { red, green, blue, }
+
+// Declaring enhanced enums
+enum Vehicle implements Comparable<Vehicle> {
+  car(tires: 4, passengers: 5, carbonPerKilometer: 400),
+  bus(tires: 6, passengers: 50, carbonPerKilometer: 800),
+  bicycle(tires: 2, passengers: 1, carbonPerKilometer: 0);
+
+  const Vehicle({
+    required this.tires,
+    required this.passengers,
+    required this.carbonPerKilometer,
+  });
+
+  final int tires;
+  final int passengers;
+  final int carbonPerKilometer;
+
+  int get carbonFootprint => (carbonPerKilometer / passengers).round();
+
+  bool get isTwoWheeled => this == Vehicle.bicycle;
+
+  @override
+  int compareTo(Vehicle other) => carbonFootprint - other.carbonFootprint;
+}
