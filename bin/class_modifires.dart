5651975cb04a819b9770334f994480e8828929aa
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-24 11:08:22 +0200

    notes for selecting modifiers

--- bin/class_modifires.dart
+++ bin/class_modifires.dart
@@ -137,4 +137,7 @@ mixin class Both {}
 class UseAsMixin with Both {}
 class UseAsSuperclass extends Both {}
 
-// @
+// * Use interface to prevent users from reusing your class's code while allowing them to re-implement its interface.
+// ? Use base to require users to reuse your class's code and ensure every instance of your class's type is an instance of that actual class or a subclass.
+// ? Use final to completely prevent a class from being extended.
+// ? Use sealed to opt in to exhaustiveness checking on a family of subtypes.
