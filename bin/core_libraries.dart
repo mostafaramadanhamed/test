c88cd5d5b149d7732a2f0117bdcad9726f68121f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-27 10:34:39 +0200

    Regular expressions

--- bin/core_libraries.dart
+++ bin/core_libraries.dart
@@ -7,5 +7,19 @@ void main()
 assert(int.parse('42') == 42);
 assert(int.parse('0x42') == 66);
 assert(double.parse('0.50') == 0.5);
+//@ Regular expressions
+// Here's a regular expression for one or more digits.
+  var digitSequence = RegExp(r'\d+');
 
+  var lettersOnly = 'llamas live fifteen to twenty years';
+  var someDigits = 'llamas live 15 to 20 years';
+
+// contains() can use a regular expression.
+  assert(!lettersOnly.contains(digitSequence));
+  assert(someDigits.contains(digitSequence));
+
+// Replace every match with another string.
+  var exedOut = someDigits.replaceAll(digitSequence, 'XX');
+  assert(exedOut == 'llamas live XX to XX years');
 }
+
