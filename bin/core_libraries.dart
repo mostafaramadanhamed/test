744a3d261a81839a8c274ea2aca541e167ecdeca
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-02-27 10:30:08 +0200

    dart core lib

---
+++ bin/core_libraries.dart
@@ -0,0 +1,11 @@
+
+// @ dart core
+// ? provides a small but critical set of built-in functionality.
+// ? This library is automatically imported into every Dart program.
+void main()
+{
+assert(int.parse('42') == 42);
+assert(int.parse('0x42') == 66);
+assert(double.parse('0.50') == 0.5);
+
+}
