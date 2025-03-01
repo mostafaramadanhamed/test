c898150b9046fa9a9d4653e9b731000772b70076
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-01 12:42:50 +0200

    lists

--- bin/core_libraries.dart
+++ bin/core_libraries.dart
@@ -21,5 +21,35 @@ assert(double.parse('0.50') == 0.5);
 // Replace every match with another string.
   var exedOut = someDigits.replaceAll(digitSequence, 'XX');
   assert(exedOut == 'llamas live XX to XX years');
+  // @ Lists
+  // Create an empty list of strings.
+  var grains = <String>[];
+  assert(grains.isEmpty);
+
+// Create a list using a list literal.
+  var fruits = ['apples', 'oranges'];
+
+// Add to a list.
+  fruits.add('kiwis');
+
+// Add multiple items to a list.
+  fruits.addAll(['grapes', 'bananas']);
+
+// Get the list length.
+  assert(fruits.length == 5);
+
+// Remove a single item.
+  var appleIndex = fruits.indexOf('apples');
+  fruits.removeAt(appleIndex);
+  assert(fruits.length == 4);
+
+// Remove all elements from a list.
+  fruits.clear();
+  assert(fruits.isEmpty);
+
+// You can also create a List using one of the constructors.
+  var vegetables = List.filled(99, 'broccoli');
+  assert(vegetables.every((v) => v == 'broccoli'));
+
 }
 
