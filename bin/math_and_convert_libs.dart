a07ab9efada7c06876e845012a85cb442102ccd4
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-05 12:23:39 +0200

    convert utf8

--- bin/math_and_convert_libs.dart
+++ bin/math_and_convert_libs.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'dart:typed_data';
 void main(){
   math();
   convert();
@@ -49,5 +50,22 @@ void convert(){
   var firstScore = scores[0];
   assert(firstScore is Map);
   assert(firstScore['score'] == 40);
+//? utf8
+List<int> utf8Bytes = [
+  0xc3, 0x8e, 0xc3, 0xb1, 0xc5, 0xa3, 0xc3, 0xa9,
+  0x72, 0xc3, 0xb1, 0xc3, 0xa5, 0xc5, 0xa3, 0xc3,
+  0xae, 0xc3, 0xb6, 0xc3, 0xb1, 0xc3, 0xa5, 0xc4,
+  0xbc, 0xc3, 0xae, 0xc5, 0xbe, 0xc3, 0xa5, 0xc5,
+  0xa3, 0xc3, 0xae, 0xe1, 0xbb, 0x9d, 0xc3, 0xb1,
+  ];
 
+  var funnyWord = utf8.decode(utf8Bytes);
+
+  assert(funnyWord == 'Îñţérñåţîöñåļîžåţîờñ');
+  Uint8List encoded = utf8.encode('Îñţérñåţîöñåļîžåţîờñ');
+
+  assert(encoded.length == utf8Bytes.length);
+  for (int i = 0; i < encoded.length; i++) {
+    assert(encoded[i] == utf8Bytes[i]);
+  }
 }
