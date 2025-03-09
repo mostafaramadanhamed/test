524f0b41224f4235d502e59058b594ec71d0fc96
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-09 14:16:39 +0200

    futures

---
+++ bin/asynchrouns.dart
@@ -0,0 +1,29 @@
+//@ Asynchronous
+//? This example shows how *not* to write asynchronous Dart code.
+//* Synchronous
+String createSOrderMessage() {
+  var order = fetchUserOrder();
+  return 'Your order is: $order';
+}
+
+Future<String> fetchSUserOrder() =>
+// Imagine that this function is
+// more complex and slow.
+Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
+
+// Asynchronous
+
+Future<String> createOrderMessage() async {
+  var order = await fetchUserOrder();
+  return 'Your order is: $order';
+}
+
+Future<String> fetchUserOrder() =>
+// Imagine that this function is
+// more complex and slow.
+Future.delayed(const Duration(seconds: 2), () => 'Large Latte');
+
+Future<void> main() async {
+  print('Fetching user order...');
+  print(await createOrderMessage());
+}
