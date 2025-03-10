80fcd41b90d58c6dc816d54d8dd2df9448030254
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-10 11:59:42 +0200

    kinds of streams

--- bin/asynchrouns.dart
+++ bin/asynchrouns.dart
@@ -50,3 +50,21 @@ Future<int> sumStream(Stream<int> stream) async {
 }
 //? error of event
 //*  You can catch the error using try-catch
+
+//? working with streams
+Future<int> lastPositive(Stream<int> stream) =>
+    stream.lastWhere((x) => x >= 0);
+//? kinds of streams
+//* Single subscription streams
+
+//>> The most common kind of stream contains a sequence of events that are parts of a larger whole.
+//>> Events need to be delivered in the correct order and without missing any of them.
+//>> This is the kind of stream you get when you read a file or receive a web request.
+//>> Such a stream can only be listened to once. Listening again later could mean missing out on initial events,
+//>> and then the rest of the stream makes no sense. When you start listening, the data will be fetched and provided in chunks.
+
+//? Broadcast streams
+//>> The other kind of stream is intended for individual messages that can be handled one at a time.
+//>> This kind of stream can be used for mouse events in a browser, for example.
+//>> You can start listening to such a stream at any time, and you get the events that are fired while you listen.
+//>> More than one listener can listen at the same time, and you can listen again later after canceling a previous subscription.
