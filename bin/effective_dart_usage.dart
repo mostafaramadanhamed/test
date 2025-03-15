16611107015000a9617be5eb79cd1df0e5ca57a2
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-15 15:13:00 +0200

    libraries

---
+++ bin/effective_dart_usage.dart
@@ -0,0 +1,7 @@
+//@ Usage
+
+//@ libraries
+//? DO use strings in part of directives
+//? DON'T import libraries that are inside the src directory of another package
+//? DON'T allow an import path to reach into or out of lib
+//? PREFER relative import paths
