4d6e2b1128373d46afdde9d1e11331314b2c5a6f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-03-16 12:49:19 +0200

    don't for Null

--- bin/effective_dart_usage.dart
+++ bin/effective_dart_usage.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 //@ Usage
 
 //@ libraries
@@ -5,3 +6,70 @@
 //? DON'T import libraries that are inside the src directory of another package
 //? DON'T allow an import path to reach into or out of lib
 //? PREFER relative import paths
+
+//@ Null
+//? DON'T explicitly initialize variables to null
+//* example
+//>> good
+Item?  bestDeal(List<Item> cart) {
+  Item? bestItem;
+
+  for (final item in cart) {
+    if (bestItem == null || item.price < bestItem.price) {
+      bestItem = item;
+    }
+  }
+
+  return bestItem;
+}
+
+class Item {
+  final num price;
+  Item(this.price);
+}
+//! bad
+Item? bestDeal2(List<Item> cart) {
+  Item? bestItem = null;
+
+  for (final item in cart) {
+    if (bestItem == null || item.price < bestItem.price) {
+      bestItem = item;
+    }
+  }
+
+  return bestItem;
+}
+//? DON'T use an explicit default value of null
+//* example
+//>> good
+
+
+void error([String? message]) {
+  stderr.write(message ?? '\n');
+}
+
+//! bad
+void error2([String? message = null]) {
+  stderr.write(message ?? '\n');
+}
+//? DON'T use true or false in equality operations
+//* example
+//>> good
+void example( bool nonNullableBool){
+
+  if (nonNullableBool) {
+//...
+}
+
+if (!nonNullableBool) {
+//...
+}
+//! bad
+if (nonNullableBool == true) {
+//...
+}
+
+if (nonNullableBool == false) {
+//...
+}}
+//?
