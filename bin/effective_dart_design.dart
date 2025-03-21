1e0f6b85eabbd389b5a6c989c8bd33e9179554d6
Mostafa Ramadan <96018237+mostafaramadanhamed@users.noreply.github.com>
2025-03-21 14:38:21 +0200

    names p2

--- bin/effective_dart_design.dart
+++ bin/effective_dart_design.dart
@@ -19,3 +19,31 @@
 //! bad
 //! buildRects
 //! InputOutputStream
+
+//? PREFER putting the most descriptive noun last
+//>> good
+//>>CssFontFaceRule       // A rule for font faces in CSS.
+//! bad
+//! RuleFontFaceCss
+
+//? CONSIDER making the code read like a sentence
+//>> good
+//>>// "If errors is empty..."
+// if (errors.isEmpty) {//   // ...
+// }
+//
+// "Hey, subscription, cancel!"
+// subscription.cancel();
+//
+// "Get the monsters where the monster has claws."
+// monsters.where((monster) => monster.hasClaws);
+//! bad
+//!// Telling errors to empty itself, or asking if it is?
+// if (errors.empty) {
+// }
+//
+// Toggle what? To what?
+// subscription.toggle();
+//
+// Filter the monsters with claws *out* or include *only* those?
+// monsters.filter((monster) => monster.hasClaws);
