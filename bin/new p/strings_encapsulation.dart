bdb9d057d1d7d3e5d2828f7678d247b844c62451
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-15 11:56:28 +0200

    w1 d3 t2 -

--- bin/new p/strings_encapsulation.dart
+++ bin/new p/strings_encapsulation.dart
@@ -4,5 +4,9 @@ void main(){
   var words='the team is tea bee cafe'.split(' ');
   print(words[1]);
   print(words.length);
-  //
+
+  //? reverse string
+  String re='row';
+  print(re.split('').reversed.join(''));
+
 }
