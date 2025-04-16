ba43b657c11d18663fb1b222d1438af6ca29527b
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-16 11:15:37 +0200

    w1 d4 t2-

--- bin/new p/inheritnce_polymorphism.dart
+++ bin/new p/inheritnce_polymorphism.dart
@@ -4,18 +4,32 @@ Dog dog=Dog();
 Cat cat =Cat();
 Animal dog2=Dog();
 Animal cat2=Cat();
+animal.makeSound();
+dog.makeSound();
+cat.makeSound();
+dog2.makeSound();
+cat2.makeSound();
 
 }
 //@ Inheritance
 class Animal {
+  //@ Polymorphism
+  void makeSound(){
 
+  }
 }
 class Dog extends Animal{
-
+  @override
+  void makeSound(){
+    print('WaWa');
+  }
 }
 
 class Cat extends Animal{
 
-
+  @override
+  void makeSound(){
+    print('mewo');
+  }
 }
 
