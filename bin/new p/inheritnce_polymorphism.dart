440729db2d3e54e65e9ba42d1c2dbe233aa0762f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-16 11:15:10 +0200

    w1 d4 t1

---
+++ bin/new p/inheritnce_polymorphism.dart
@@ -0,0 +1,21 @@
+void main(){
+Animal animal=Animal();
+Dog dog=Dog();
+Cat cat =Cat();
+Animal dog2=Dog();
+Animal cat2=Cat();
+
+}
+//@ Inheritance
+class Animal {
+
+}
+class Dog extends Animal{
+
+}
+
+class Cat extends Animal{
+
+
+}
+
