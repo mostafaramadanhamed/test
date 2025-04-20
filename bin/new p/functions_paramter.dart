92d75e99e0b6b7f9430498434681a4ba6fbf012f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-20 11:37:51 +0200

    w2 d1 t2-

--- bin/new p/functions_paramter.dart
+++ bin/new p/functions_paramter.dart
@@ -1,12 +1,32 @@
 void main(){
   print(greeting('Alice')); // Hello, Alice!
-  print(greeting('Bob', title: 'Dr.')); //
+  print(greeting('Bob', title: 'Dr.'));
+  print(calculator(10, 5)); // Default: add → 15.0
+  print(calculator(10, 5, operation: 'subtract')); // 5.0
+  print(calculator(10, 5, operation: 'multiply')); // 50.0
+  print(calculator(10, 5, operation: 'divide'));   // 2/
 }
 String greeting(String name, {String title = ''}) {
   if (title.isEmpty) {
     return 'Hello, $name!';
   } else {
     return 'Hello, $title $name!';
+  }
+}
+
+double calculator(double a, double b, {String operation = 'add'}) {
+  switch (operation) {
+    case 'add':
+      return a + b;
+    case 'subtract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b == 0) throw Exception('Cannot divide by zero');
+      return a / b;
+    default:
+      throw Exception('Invalid operation: $operation');
   }
 }
 
